2db9ebe24bfaac9e834c3210a7ef3443ec765f04
clockwerkz <clockwerkz@gmail.com>
2019-08-13 00:01:41 -0700

    working with arrays

--- pluralsight/gradebook/src/GradeBook/Program.cs
+++ pluralsight/gradebook/src/GradeBook/Program.cs
@@ -6,8 +6,18 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            string name = args.Length > 0 ? args[0] : "Default";
-            Console.WriteLine($"Hello, {name}!");
+            // double x = 34.1;
+            // double y = 56.4;
+            // double res = x + y;
+            // Console.WriteLine($"{x} + {y} = {res}");
+
+            var numbers = new[] { 92.7, 90.3, 45.3 };
+
+            if (args.Length > 0) {
+                Console.WriteLine($"Hello, {args[0]}!");
+            } else {
+                Console.WriteLine("Hello!");
+            }
         }
     }
 }
