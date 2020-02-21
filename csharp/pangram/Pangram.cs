e7f39361e579a0b491a1d798d63b865abba1b561
clockwerkz <clockwerkz@gmail.com>
2020-02-20 23:01:06 -0800

    Adds pangram exercise

--- csharp/pangram/Pangram.cs
+++ csharp/pangram/Pangram.cs
@@ -4,6 +4,22 @@ public static class Pangram
 {
     public static bool IsPangram(string input)
     {
-        throw new NotImplementedException();
+        if (input.Length < 26) return false;
+        input = input.ToLower();
+        int[] alphabet = new int[26];
+
+        for (int i=0; i<input.Length; i++)
+        {
+            int letterIndex = (int)input[i] - 97;
+            if (letterIndex >= 0 && letterIndex <= 25)
+            {
+                alphabet[letterIndex] = 1;
+            }
+        }
+        for (int i=0; i < alphabet.Length; i++)
+        {
+            if (alphabet[i] == 0) return false;
+        }
+        return true;
     }
 }
