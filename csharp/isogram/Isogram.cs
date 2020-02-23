8fe40e2177a310b8fdf2968c95c1bd479e7d0f4f
clockwerkz <clockwerkz@gmail.com>
2020-02-23 15:33:35 -0800

    Solved isogram and added acronym challenge

--- csharp/isogram/Isogram.cs
+++ csharp/isogram/Isogram.cs
@@ -1,19 +1,22 @@
 ﻿using System;
-using System.Collection.Generic;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 public static class Isogram
 {
     public static bool IsIsogram(string word)
     {
-        Dictionary<string, int> hash = new Dictionary<string, int>();
-        for (int i=0; i<word.Length; i++)
+        Regex rgx = new Regex("[^a-z]");
+        string strippedWord= rgx.Replace(word.ToLower(), "");
+        Dictionary<char, int> hash = new Dictionary<char, int>();
+        for (int i=0; i<strippedWord.Length; i++)
         {
-            if (hash.ContainsKey(word[i]))
+            if (hash.ContainsKey(strippedWord[i]))
             {
                 return false;
             }
             else
-                hash.Add(word[i], 1);
+                hash.Add(strippedWord[i], 1);
         }
         return true;
     }
