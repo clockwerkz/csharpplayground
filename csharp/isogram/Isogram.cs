89e950b22beb3ee91d7b58bbb76995eab9b10582
Carlos Fins <clockwerkz@gmail.com>
2020-02-23 10:28:34 -0800

    adds Isogram solution
    TODO: Run tests on other computer

---
+++ csharp/isogram/Isogram.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collection.Generic;
+
+public static class Isogram
+{
+    public static bool IsIsogram(string word)
+    {
+        Dictionary<string, int> hash = new Dictionary<string, int>();
+        for (int i=0; i<word.Length; i++)
+        {
+            if (hash.ContainsKey(word[i]))
+            {
+                return false;
+            }
+            else
+                hash.Add(word[i], 1);
+        }
+        return true;
+    }
+}
