ba3e900352428c5aeeb25ee5db534b3eaca18e89
clockwerkz <clockwerkz@gmail.com>
2020-07-13 23:52:09 -0700

    Initial project setup - Tim Corey's Dependency Injection with AutoFac video

---
+++ DependencyInjectionDemo/DemoLibrary/Utilities/DataAccess.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary.Utilities
+{
+    class DataAccess
+    {
+        internal void SaveData(string name)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine($"Saving { name }");
+            Console.ResetColor();
+        }
+
+        internal void LoadData()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Loading Data");
+            Console.ResetColor();
+        }
+    }
+}
---
+++ DependencyInjectionDemo/DemoLibrary/Utilities/Logger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary.Utilities
+{
+    class Logger
+    {
+        internal void Log(string message)
+        {
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine($"Logging: { message }");
+            Console.ResetColor();
+        }
+    }
+}
